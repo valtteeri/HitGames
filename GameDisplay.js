32ee34be2262d52453d34e737d3363debfdfe771
Arttu Koskenniemi <t9koar01@students.oamk.fi>
2021-04-20 05:09:22 +0300

    Reworking Game item generation

--- GameDisplay.js
+++ GameDisplay.js
@@ -1,24 +1,67 @@
+let testgames = [
+    {
+        name: 'Placeholder name',
+        image: 'placeholder.png',
+        description: 'game where you play with blocks',
+        rating: '4.3',
+        popularity: '324'
+    },
+    {
+        name: 'Placeholder name',
+        image: 'placeholder.png',
+        description: 'game where you place bombs',
+        rating: '2.4',
+        popularity: '528'
+    },
+    {
+        name: 'Placeholder name',
+        image: 'placeholder.png',
+        description: 'game where you find bombs',
+        rating: '1.7',
+        popularity: '86'
+    }
+]
+
 function generateGames() {
     let GameList = document.querySelector(".Gamecontainer");
-    for (let i=0; i < 5; i++) {
-        document.querySelector(".Gamecontainer").innerHTML += `
-            <div class = "aGame">
-                <div class = "GameLogo">
-                    <img src="gamelogo.png" width="125px">
-                </div>
-                <div class = "Gametitle">
-                    Game Title
-                </div>
-                <div class = "GameDescription">
-                    Describe <br> game <br> here
-                </div>
-                <div class= "GameRating">
-                    ★★★☆☆ <br>
-                    <button class= "PlayButton"> Play </button>
-                </div>
+    Object.values(testgames).map(game => {
+        GameList.innerHTML += `
+        <div class = "aGame">
+            <div class = "GameLogo">
+                <img src="${game.image}" width="125px">
             </div>
-            <hr>`
-    }
+            <div class = "Gametitle">
+                ${game.name}
+            </div>
+            <div class = "GameDescription">
+                ${game.description}
+            </div>
+            <div class= "GameRating">
+                 ${game.rating} <i class="fas fa-star"></i> <br>
+                <button class= "PlayButton"> Play </button>
+            </div>
+        </div>
+        <hr>`;
+    });
+    // for (let i=0; i < testgames.length; i++) {
+    //     GameList.innerHTML += `
+    //         <div class = "aGame">
+    //             <div class = "GameLogo">
+    //                 <img src="gamelogo.png" width="125px">
+    //             </div>
+    //             <div class = "Gametitle">
+    //                 Game Title
+    //             </div>
+    //             <div class = "GameDescription">
+    //                 Describe <br> game <br> here
+    //             </div>
+    //             <div class= "GameRating">
+    //                 ★★★☆☆ <br>
+    //                 <button class= "PlayButton"> Play </button>
+    //             </div>
+    //         </div>
+    //         <hr>`
+    // }
 
 }
 
