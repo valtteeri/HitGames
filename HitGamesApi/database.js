e9871a04c7c0f68cb47f4056d63efb9e3037b9dd
valtteeri <hietalavaltteri@gmail.com>
2021-05-04 00:47:33 +0300

    Update database.js

--- HitGamesApi/database.js
+++ HitGamesApi/database.js
@@ -2,7 +2,7 @@ const mysql = require('mysql');
 const connection = mysql.createPool({
   host: 'localhost',
   user: 'root',
-  password: 'test',
-  database: 'hitgamesdb'
+  password: '1234',
+  database: 'HitGames'
 });
 module.exports = connection;
